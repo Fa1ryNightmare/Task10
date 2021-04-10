7c1964cc7dd4f4213931f47bc7fe9d3f05fdc059
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-04-10 21:02:51 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Task10
+{
+    class Matrix
+    {
+        public double[,] elements;
+        public int RowsCount { get; private set; }
+        public int ColumnsCount { get; private set; }
+
+        public Matrix(int RowsCount, int ColumnsCount)
+        {
+            elements = new double[RowsCount, ColumnsCount];
+            this.RowsCount = RowsCount;
+            this.ColumnsCount = ColumnsCount;
+        }
+
+        public static Matrix GetEmpty(int RowsCount, int ColumnsCount)
+        {
+            return new Matrix(RowsCount, ColumnsCount);
+        }
+
+        public static Matrix Add(Matrix MatrixA, Matrix MatrixB)
+        {
+            if (MatrixA.RowsCount != MatrixB.RowsCount || MatrixA.ColumnsCount != MatrixB.ColumnsCount)
+            {
+                throw new MatrixException($"Ошибка\nРазмерность первой матрицы {MatrixA.RowsCount}x{MatrixA.ColumnsCount}, " +
+                    $"Размерность второй матрицы {MatrixB.RowsCount}x{MatrixB.ColumnsCount}");
+            }
+
+            Matrix MatrixC = new Matrix(MatrixA.RowsCount, MatrixA.ColumnsCount);
+
+            for (int i = 0; i < MatrixC.RowsCount; i++)
+            {
+                for (int j = 0; j < MatrixC.ColumnsCount; j++)
+                {
+                    MatrixC.elements[i, j] = MatrixA.elements[i, j] + MatrixB.elements[i, j];
+                }
+            }
+            return MatrixC;
+        }
+
+        public static Matrix Sub(Matrix MatrixA, Matrix MatrixB)
+        {
+            if (MatrixA.RowsCount != MatrixB.RowsCount || MatrixA.ColumnsCount != MatrixB.ColumnsCount)
+            {
+                throw new MatrixException($"Ошибка\nРазмерность первой матрицы {MatrixA.RowsCount}x{MatrixA.ColumnsCount}, " +
+                    $"Размерность второй матрицы {MatrixB.RowsCount}x{MatrixB.ColumnsCount}");
+            }
+
+            Matrix MatrixC = new Matrix(MatrixA.RowsCount, MatrixA.ColumnsCount);
+
+            for (int i = 0; i < MatrixC.RowsCount; i++)
+            {
+                for (int j = 0; j < MatrixC.ColumnsCount; j++)
+                {
+                    MatrixC.elements[i, j] = MatrixA.elements[i, j] - MatrixB.elements[i, j];
+                }
+            }
+            return MatrixC;
+        }
+
+        public static Matrix Mul(Matrix MatrixA, Matrix MatrixB)
+        {
+            if (MatrixA.ColumnsCount != MatrixB.RowsCount)
+            {
+                throw new MulMatrixException($"Ошибка\nРазмерность первой матрицы {MatrixA.RowsCount}x{MatrixA.ColumnsCount}, " +
+                    $"Размерность второй матрицы {MatrixB.RowsCount}x{MatrixB.ColumnsCount}");
+            }
+
+            Matrix MatrixC = new Matrix(MatrixA.RowsCount, MatrixB.ColumnsCount);
+
+            for (int i = 0; i < MatrixA.RowsCount; i++)
+            {
+                for (int j = 0; j < MatrixB.ColumnsCount; j++)
+                {
+                    for (int k = 0; k < MatrixA.ColumnsCount; k++)
+                    {
+                        MatrixC.elements[i, j] += MatrixA.elements[i, k] * MatrixB.elements[k, j];
+                    }
+                }
+            }
+            return MatrixC;
+        }
+    }
+
+    class MatrixException : Exception
+    {
+        public MatrixException(string message) : base(message)
+        {
+        }
+    }
+
+    class MulMatrixException : Exception
+    {
+        public MulMatrixException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Кол-во строк первой матрицы: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кол-во столбцов первой матрицы: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Matrix matrix1 = new Matrix(n, m);
+            FillMatrix(matrix1);
+
+            Console.Write("Кол-во строк второй матрицы: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кол-во столбцов второй матрицы: ");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            Matrix matrix2 = new Matrix(n, m);
+            FillMatrix(matrix2);
+
+            Console.WriteLine("Первая матрица: ");
+            PrintMatrix(matrix1);
+            Console.WriteLine("Вторая матрица: ");
+            PrintMatrix(matrix2);
+
+            try
+            {
+                Console.WriteLine("Результат сложения матриц: ");
+                PrintMatrix(Matrix.Add(matrix1, matrix2));
+            }
+            catch (MatrixException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Результат вычитания матриц: ");
+                PrintMatrix(Matrix.Sub(matrix1, matrix2));
+            }
+            catch (MatrixException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Результат умножения матриц: ");
+                PrintMatrix(Matrix.Mul(matrix1, matrix2));
+            }
+            catch (MulMatrixException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Write("Кол-во строк нулевой матрицы: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кол-во столбцов нулевой матрицы: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Нулевая матрица: ");
+            PrintMatrix(Matrix.GetEmpty(n, m));
+
+            Console.ReadKey();
+        }
+        
+        static void PrintMatrix(Matrix matrix)
+        {
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                for (int j = 0; j < matrix.ColumnsCount; j++)
+                {
+                    Console.Write($"{matrix.elements[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void FillMatrix(Matrix matrix)
+        {
+            Console.WriteLine("Заполните матрицу: ");
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                for (int j = 0; j < matrix.ColumnsCount; j++)
+                {
+                    Console.Write($"M[{i + 1},{j + 1}] = ");
+                    matrix.elements[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+        }
+    }
+}
